bd44894e1b5e103c94e2b1dd48918d3d9286b952
M F <matheus@mbmarketing.com.br>
2024-11-18 22:28:36 -0300

    TP4

--- app/services/FastAPI/main.py
+++ app/services/FastAPI/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
+from routers.items import router
 
 app = FastAPI()
+app.include_router(router)
 
 @app.get("/")
 async def read_root():
     return {"message" : "Hello World"}
 
-@app.post("/items/")
-async def create_item(name: str):
-    return {"name": name}
--- app/services/FastAPI/routers/items.py
+++ app/services/FastAPI/routers/items.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from transformers import pipeline
+from pydantic import BaseModel
 
 router = APIRouter()
+
+class AutoCompleteModel(BaseModel):
+    phrase: str
 
 @router.get("/")
 async def read_root():
     return {"message": "Hello World"}
 
-@router.post("/items/")
-async def create_item(name: str):
-    return {"name": name}
+def generate_response(message: str):
+    generator = pipeline("text-generation", model="gpt2-large")
+    return generator(message)
+
+@router.post("/autocomplete")
+async def autocomplete(body: AutoCompleteModel):
+    if not body.phrase:
+        raise HTTPException(status_code=400, detail="Invalid prompt.")
+    response = generate_response(body.phrase)
+    return {"assistant": response}
